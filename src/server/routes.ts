29ace3062d56827b339f7a7d64ab9783713e2157
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 13:08:07 -0500

    secured backend info

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,22 @@
 import * as express from 'express';
+import DB from "./db";
+// import config from "./config";
 
 const router = express.Router();
 
-router.get('/hello', (req, res, next) => {
+router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
+
+router.get("/api/blogs", async (req, res) => {
+    // DB.Blogs.all  // comes from server/db/index => imported at the top
+    try {
+        let blogs = await DB.Blogs.all();
+        res.json(blogs);
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
 
 export default router;
--- src/server/server.ts
+++ src/server/server.ts
@@ -9,8 +9,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(config.app.prefix, apiRouter);
-
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 app.listen(config.app.port, () => console.log(`Server listening on port: ${config.app.port}`));
+
